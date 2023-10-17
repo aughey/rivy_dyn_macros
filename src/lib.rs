9b324f67a827d748146b713b8b55ba14b7400729
John Aughey <jha@aughey.com>
2023-10-17 17:52:27 +0000

    first macros cut

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use proc_macro::TokenStream;
 #[proc_macro_attribute]
 pub fn node(attr: TokenStream, item: TokenStream) -> TokenStream {
     // parse item as a function
-    let ast = syn::parse::<syn::Item>(item).unwrap();
+    let ast = syn::parse::<syn::ItemFn>(item).unwrap();
 
     process_function(ast, attr.into()).unwrap().into()
 }

5bd9d1118623086918fe21b8b1f723a7f52cb119
John Aughey <jha@aughey.com>
2023-10-18 04:00:23 +0000

    almost ready to complete cycle for async runners

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,12 @@ pub fn node(attr: TokenStream, item: TokenStream) -> TokenStream {
 
     process_function(ast, attr.into()).unwrap().into()
 }
+
+#[proc_macro_attribute]
+pub fn stream_type(_attr: TokenStream,item: TokenStream) -> TokenStream {
+    // parse item as a function
+    // let ast = syn::parse::<syn::Struc>(item).unwrap();
+
+    // process_function(ast, attr.into()).unwrap().into()
+    item
+}

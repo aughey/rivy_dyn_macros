fc54ef95c29771105dd2f0b4c25661273f39f996
John Aughey <jha@aughey.com>
2023-10-22 06:20:49 +0000

    iterators

--- src/lib.rs
+++ src/lib.rs
@@ -17,3 +17,12 @@ pub fn stream_type(_attr: TokenStream,item: TokenStream) -> TokenStream {
     // process_function(ast, attr.into()).unwrap().into()
     item
 }
+
+#[proc_macro_attribute]
+pub fn iterator_type(_attr: TokenStream,item: TokenStream) -> TokenStream {
+    // parse item as a function
+    // let ast = syn::parse::<syn::Struc>(item).unwrap();
+
+    // process_function(ast, attr.into()).unwrap().into()
+    item
+}
